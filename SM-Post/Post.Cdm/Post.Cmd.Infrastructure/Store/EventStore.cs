ccf4a82e363a5736305c1290845ee2f033ccfbdf
tlkhanh <longkhanhta@gmail.com>
2024-08-23 23:36:50 +0700

    feat: kafka producer

--- SM-Post/Post.Cdm/Post.Cmd.Infrastructure/Store/EventStore.cs
+++ SM-Post/Post.Cdm/Post.Cmd.Infrastructure/Store/EventStore.cs
@@ -2,22 +2,20 @@
 using CQRS.Core.Events;
 using CQRS.Core.Exceptions;
 using CQRS.Core.Infrastructure;
+using CQRS.Core.Producers;
 using Post.Cmd.Domain.Aggregates;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Post.Cmd.Infrastructure.Store
 {
     public class EventStore : IEventStore
     {
         private readonly IEventStoreRepository _eventStoreRepository;
+        private readonly IEventProducer _eventProducer;
 
-        public EventStore(IEventStoreRepository eventStoreRepository)
+        public EventStore(IEventStoreRepository eventStoreRepository, IEventProducer eventProducer)
         {
             _eventStoreRepository = eventStoreRepository;
+            _eventProducer = eventProducer;
         }
         public async Task<List<BaseEvent>> GetEventsAsync(Guid aggregateId)
         {
@@ -53,6 +51,8 @@ namespace Post.Cmd.Infrastructure.Store
                     EventData = @event
                 };
                 await _eventStoreRepository.SaveAsync(eventModel);
+                var topic = Environment.GetEnvironmentVariable("KAFKA_TOPIC");
+                await _eventProducer.ProduceAsync(topic, @event);
             }
         }
     }
