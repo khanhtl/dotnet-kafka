f39b5dbb12ff424a364128bff5a6a7c06eb0bf67
tlkhanh <longkhanhta@gmail.com>
2024-08-24 11:13:40 +0700

    feat: domain layer

---
+++ SM-Post/Post.Cdm/Post.Cmd.Domain/Repositories/ICommentRepository.cs
@@ -0,0 +1,11 @@
+﻿using Post.Cmd.Domain.Entities;
+
+namespace Post.Cmd.Domain.Repositories;
+
+public interface ICommentRepository
+{
+    Task CreateAsync(CommentEntity comment);
+    Task UpdateAsync(CommentEntity comment);
+    Task<CommentEntity> GetByIdAsync(Guid commentId);
+    Task DeleteAsync(Guid commentId);
+}
---
+++ SM-Post/Post.Cdm/Post.Cmd.Domain/Repositories/IPostRepository.cs
@@ -0,0 +1,17 @@
+﻿using Post.Cmd.Domain.Entities;
+
+namespace Post.Cmd.Domain.Repositories
+{
+    public interface IPostRepository
+    {
+        Task CreateAsync(PostEntity post);
+        Task UpdateAsync(PostEntity post);
+        Task DeleteAsync(Guid postId);
+        Task<PostEntity> GetByIdAsync(Guid postId);
+        Task<List<PostEntity>> GetAllAsync();
+        Task<List<PostEntity>> GetByAuthorAsync(string author);
+        Task<List<PostEntity>> GetWithLikesAsync(int numberOfList);
+        Task<List<PostEntity>> GetWithCommentsAsync();
+
+    }
+}
